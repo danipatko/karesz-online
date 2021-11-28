84813f752a61d8840ad9a2a65d9ab87c93bbda02
danipatko <p8.pada@gmail.com>
2021-11-28 22:35:09 +0100

    replacing/converting C# functions

--- src/lib/karesz/karesz-dotnet.ts
+++ src/lib/karesz/karesz-dotnet.ts
@@ -41,10 +41,6 @@ export const run = (filename:string='/mnt/c/Users/Dani/home/Projects/karesz-onli
             }
         });
 
-        mono.on('message', (data:Buffer) => {
-            console.log(`message: ${data}`);
-        });
-
         mono.once('exit', (code, signal) => res({ code:code, signal:signal }));
     });
 }
@@ -53,14 +49,17 @@ export const tryrun = async() => {
     // const rr = await run();
     //console.log(rr);
 
-    replaceKareszFunctions();
+    replaceKareszFunctions(test);
 
     console.log('exited');
 }
 
 const UTIL_FUNCTIONS_FOR_CSHARP = 
-`public static bool stdin(string command, string match){Console.WriteLine($"in:{command}");string value = Console.ReadLine();return value == match;}
-public static void stdout(string command) {Console.WriteLine($"out:{command}");}`;
+`
+public static bool stdin(string command,string match){Console.WriteLine($"in:{command}");string value=Console.ReadLine();return value==match;}
+public static int stdin(string command){Console.WriteLine($"in:{command}");string value=Console.ReadLine();return int.Parse(value);}
+public static void stdout(string command){Console.WriteLine($"out:{command}");}
+`;
 
 const test = `using System;
 using System.Collections.Generic;
@@ -141,24 +140,52 @@ namespace Karesz
 }
 `;
 
-const replace = (regex:RegExp, v:string) => {
-    const match = regex.exec(v);
-    if(!match)  return;
-
-    match.forEach(x => {
-        console.log(x);
+const replaceNumbers = (s:string, match:RegExp, key:string) => {
+    match.exec(s).map(x => {
+        s = s.replaceAll(x, key.replaceAll(':x:', x.match(/[1-9]/gm).join()));
     });
+    return s;
 }
 
-export const replaceKareszFunctions = (filename:string='/mnt/c/Users/Dani/home/Projects/karesz-online/testing/Program.exe') => {
-    
+const replace = (args:object, s:string):string => {
+    for(const key in args) 
+        s = args[key].x ? replaceNumbers(s, args[key].x, key) : s.replaceAll(args[key], key);
+    return s;
+}
+
+const insert = (s:string, toAdd:string, at:number):string => {
+    return [s.slice(0, at), toAdd, s.slice(at)].join('');
+}
+
+export const replaceKareszFunctions = (str:string) => {
     // Instert utility functions after 
     // 'public partial class Form1 : Form {'
-    
-    const match = /public\s*partial\s*class\s*Form1\s*:\s*Form[\n\r\s]+{/gm.exec(test);
+
+    str = test;
+    const match = /public\s*partial\s*class\s*Form1\s*:\s*Form[\n\r\s]+{/gm.exec(str);
     if(! match)
         return false;
-    const index = (test.indexOf(match[0]) + match[0].length);
-
     
+    str = insert(str, UTIL_FUNCTIONS_FOR_CSHARP, str.indexOf(match[0]) + match[0].length);
+    
+    str = replace({
+        'stdout("step")':/Lépj\s*\(s*\)/gm,
+        'stdout("turn 1")':/Fordulj_jobbra\s*\(s*\)/gm,
+        'stdout("turn -1")':/Fordulj_balra\s*\(s*\)/gm,
+        'stdout("pickup")':/Vegyél_fel_egy_kavicsot\s*\(s*\)/gm,
+        'stdout("place")':/Tegyél_le_egy_kavicsot\s*\(s*\)/gm,
+        'stdin("up","0")':/Északra_néz\s*\(s*\)/gm,
+        'stdin("down","2")':/Délre_néz\s*\(s*\)/gm,
+        'stdin("left","3")':/Keletre_néz\s*\(s*\)/gm,
+        'stdin("right","1")':/Nyugatra_néz\s*\(s*\)/gm,
+        'stdin("look")':/Merre_néz\s*\(s*\)/gm,
+        'stdin("isrock","true")':/Van_e_itt_kavics\s*\(s*\)/gm,
+        'stdin("under")':/Mi_van_alattam\s*\(s*\)/gm,
+        'stdin("wallahead","true")':/Van_e_előttem_fal\s*\(s*\)/gm,
+        'stdin("outofbounds","true")':/Kilépek_e_a_pályáról\s*\(s*\)/gm,
+        'stdout("turn :x:")':{ x: /Fordulj\s*\(\d*\)/gm },
+    }, str); 
+
+    console.log(str);
+    return str;
 }
--- testing/Program.cs
+++ testing/Program.cs
@@ -1,30 +1,81 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
 
 namespace Karesz
 {
-    public class Program
+    public partial class Form1 : Form
     {
-        public static bool stdin(string command, string match){Console.WriteLine($"in:{command}");string value = Console.ReadLine();return value == match;}
-        public static void stdout(string command) {Console.WriteLine($"out:{command}");}
+public static bool stdin(string command,string match){Console.WriteLine($"in:{command}");string value=Console.ReadLine();return value==match;}
+public static int stdin(string command){Console.WriteLine($"in:{command}");string value=Console.ReadLine();return int.Parse(value);}
+public static void stdout(string command){Console.WriteLine($"out:{command}");}
 
-        public static void Main(string[] args)
+        bool Tudok_e_menni()
         {
-            Console.WriteLine("in:");
-            string r = Console.ReadLine();
-            Console.WriteLine("Got: " + r);
+            return !stdin("wallahead","true") && !stdin("outofbounds","true");
+        }
+        void MenyjAmígTudsz_ésKavics()
+        {
+            while(Tudok_e_menni())
+            {
+                stdout("step");
+                if(stdin("isrock","true"))
+                {
+                    stdout("pickup");
+                    kavicsok++;
+                }
+            }
+        }
+        void goRightUp()
+        {
+            MenyjAmígTudsz_ésKavics();
+            stdout("turn -1");
+            MenyjAmígTudsz_ésKavics();
+            Fordulj(2);
+        }
+        int kavicsok = 0;
+        void FELADAT()
+        {
 
-            Console.WriteLine("in:");
-            r = Console.ReadLine();
-            Console.WriteLine("Got: " + r);
+            bool fut = true;
+            int irány = 1;
+            goRightUp();
+            while (fut)
+            {
+                MenyjAmígTudsz_ésKavics();
+                Fordulj(irány);
+                if(!Tudok_e_menni())
+                {
+                    fut = false;
+                }
+                else
+                {
+                    stdout("step");
+                    if (stdin("isrock","true"))
+                    {
+                        stdout("pickup");
+                        kavicsok++;
+                    }
+                    Fordulj(irány);
+                }
 
-            Console.WriteLine("in:");
-            r = Console.ReadLine();
-            Console.WriteLine("Got: " + r);
-
-            Console.WriteLine("in:");
-            r = Console.ReadLine();
-            Console.WriteLine("Got: " + r);
+                if(irány == 1)
+                {
+                    irány = -1;
+                }
+                else
+                {
+                    irány = 1;
+                }
+            }
+            MessageBox.Show(Convert.ToString(kavicsok) + " kavicsot tudtam felszedni");
         }
     }
 }
